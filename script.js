179c7a0deb2db2d61aad7307ce29e79009e90044
edcarcruz <edcarcruz@gmail.com>
2023-09-30 13:50:34 -0400

    added jQuery and death animation

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const petName = prompt('What is your pet\'s name?');
+const petName = prompt("What is your pet's name?");
 function startGame() {
+  document.getElementById("pet-name").textContent = petName;
+}
 
-    document.getElementById('pet-name').textContent = petName;
-  
-  }
-
-  window.addEventListener("load", startGame)
-
+window.addEventListener("load", startGame);
 
 class Tamagotchi {
   constructor(name) {
@@ -15,6 +12,7 @@ class Tamagotchi {
     this.sleepiness = 5;
     this.boredom = 5;
     this.age = 0;
+    this.isDead = false;
   }
 
   feed() {
@@ -30,22 +28,22 @@ class Tamagotchi {
   }
   evolvePet() {
     // Get the pet's age
-    const petAge = document.getElementById('age').textContent;
-  
-    if (petAge === '5') {
+    const petAge = document.getElementById("age").textContent;
+
+    if (petAge === "5") {
       // Change the pet's image to the image of the evolved pet
-      document.getElementById('pet-image').src = './img/evolved-pet.png';
-      alert('You have evolved your pet!');
+      document.getElementById("pet-image").src = "./img/evolved-pet.png";
+      alert("You have evolved your pet!");
     }
   }
   evolvePet2() {
     // Get the pet's age
-    const petAge = document.getElementById('age').textContent;
-  
-    if (petAge === '10') {
+    const petAge = document.getElementById("age").textContent;
+
+    if (petAge === "10") {
       // Change the pet's image to the image of the evolved pet
-      document.getElementById('pet-image').src = './img/final_form.png';
-      alert('Your pet has evolved to its final form!');
+      document.getElementById("pet-image").src = "./img/final_form.png";
+      alert(`${this.name} has evolved to its final form!`);
     }
   }
 
@@ -60,39 +58,45 @@ class Tamagotchi {
       this.die();
     }
   }
-  
 
   die() {
-    alert(`${this.name} has died!`);
+    if (!this.isDead) {
+      this.isDead = true;
+      alert(`${this.name} has died!`);
+      $("body").fadeOut(1000, function () {
+      $("body").html("<img src='./img/game-over.png'>").fadeIn(2000);
+        });
+      
+    }
   }
 }
 const tamagotchi = new Tamagotchi(petName);
 
 function updateMetrics() {
-    // Update the values of the metrics
-    document.getElementById("hunger").innerHTML = tamagotchi.hunger;
-    document.getElementById("sleepiness").innerHTML = tamagotchi.sleepiness;
-    document.getElementById("boredom").innerHTML = tamagotchi.boredom;
-    document.getElementById("age").innerHTML = tamagotchi.age;
-  }
-  
-  setInterval(updateMetrics, 100);
-  
-  setInterval(function () {
-    tamagotchi.update();
-    tamagotchi.evolvePet();
-    tamagotchi.evolvePet2();
-  }, 10000);
-  
-  // Add event listeners to the buttons
-  document.getElementById("feed-button").addEventListener("click", () => {
-    tamagotchi.feed();
-  });
-  
-  document.getElementById("sleep-button").addEventListener("click", () => {
-    tamagotchi.sleep();
-  });
-  
-  document.getElementById("play-button").addEventListener("click", () => {
-    tamagotchi.play();
-  });
+  // Update the values of the metrics
+  document.getElementById("hunger").innerHTML = tamagotchi.hunger;
+  document.getElementById("sleepiness").innerHTML = tamagotchi.sleepiness;
+  document.getElementById("boredom").innerHTML = tamagotchi.boredom;
+  document.getElementById("age").innerHTML = tamagotchi.age;
+}
+
+setInterval(updateMetrics, 100);
+
+setInterval(function () {
+  tamagotchi.update();
+  tamagotchi.evolvePet();
+  tamagotchi.evolvePet2();
+}, 10000);
+
+// Add event listeners to the buttons
+document.getElementById("feed-button").addEventListener("click", () => {
+  tamagotchi.feed();
+});
+
+document.getElementById("sleep-button").addEventListener("click", () => {
+  tamagotchi.sleep();
+});
+
+document.getElementById("play-button").addEventListener("click", () => {
+  tamagotchi.play();
+});
